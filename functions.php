28816cc4fd751e8d597c73a18560411c8a7aefaa
araaiko <araaiko.122@gmail.com>
2021-01-14 23:05:37 +0900

    htmlファイルをindex.phpに変換する作業 完了

--- functions.php
+++ functions.php
@@ -18,5 +18,13 @@ function my_setup()
 }
 
 add_action('after_setup_theme', 'my_setup');
-// End テーマのセットアップ
+
+// CSSとJavaScriptの読み込みに必要な記述
+function my_script_init()
+{
+wp_enqueue_style('fontawesome', 'https://use.fontawesome.com/releases/v5.8.2/css/all.css', array(), '5.8.2', 'all');
+wp_enqueue_style('my', get_template_directory_uri() . '/css/style.css', array(), '1.0.0', 'all');
+wp_enqueue_script('my', get_template_directory_uri() . '/js/script.js', array( 'jquery' ), '1.0.0', true);
+}
+add_action('wp_enqueue_scripts', 'my_script_init');
 ?>
